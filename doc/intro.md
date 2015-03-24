f2f918e14460e0b175a7416712b777e0f4084a6e
Tim Macdonald <tsmacdonald@gmail.com>
2015-03-23 23:08:13 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to frontwards-hsilop-notation
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/frontwards_hsilop_notation/core.clj
@@ -0,0 +1,33 @@
+(ns frontwards-hsilop-notation.core)
+
+(def operators
+  {"+" +
+   "-" -
+   "/" /
+   "*" *})
+
+(def operator? (set (keys operators)))
+
+(def numeric? (partial re-matches #"(\d*\.)?\d+"))
+
+(def parse-number read-string)
+
+(defn- invoke-operator
+  [operator [rh lh & rest]]
+  (when-not (and rh lh)
+    (throw (IllegalArgumentException. "Not enough juice in the stack")))
+  (conj rest
+        (operator lh rh)))
+
+(defn eval-rpn
+  ([tokens] (eval-rpn tokens (list)))
+  ([[token & rest] stack]
+   (if token
+     (cond
+       (operator? token) (eval-rpn rest
+                                   (invoke-operator (operators token) stack))
+       (numeric? token)  (eval-rpn rest
+                                   (conj stack (parse-number token)))
+       :else             (throw
+                          (IllegalArgumentException. (str "Unexpected token: " (prn-str token)))))
+     stack)))
